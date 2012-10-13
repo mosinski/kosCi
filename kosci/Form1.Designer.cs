7a179e7408a2b627224021da0f0b0e01e82136a0
Miłosz Osiński <mylosz@yahoo.pl>
2012-10-14 00:28:31 +0200

    dodanie przycisków i początek silnika dla wersji klasycznej gry

--- kosci/Form1.Designer.cs
+++ kosci/Form1.Designer.cs
@@ -28,22 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.graToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.rodzajGryToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.wersjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.wersjaKarcianaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ilośćRundToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.wyjdźToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.wynikiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.autorzyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.kostka1 = new System.Windows.Forms.CheckBox();
             this.kostka2 = new System.Windows.Forms.CheckBox();
             this.kostka3 = new System.Windows.Forms.CheckBox();
             this.kostka4 = new System.Windows.Forms.CheckBox();
             this.kostka5 = new System.Windows.Forms.CheckBox();
+            this.rzuc = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -82,14 +84,14 @@
             // wersjaToolStripMenuItem
             // 
             this.wersjaToolStripMenuItem.Name = "wersjaToolStripMenuItem";
-            this.wersjaToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
-            this.wersjaToolStripMenuItem.Text = "Wersja ";
+            this.wersjaToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.wersjaToolStripMenuItem.Text = "Wersja Klasyczna";
             this.wersjaToolStripMenuItem.Click += new System.EventHandler(this.wersjaToolStripMenuItem_Click);
             // 
             // wersjaKarcianaToolStripMenuItem
             // 
             this.wersjaKarcianaToolStripMenuItem.Name = "wersjaKarcianaToolStripMenuItem";
-            this.wersjaKarcianaToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.wersjaKarcianaToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
             this.wersjaKarcianaToolStripMenuItem.Text = "Wersja Karciana";
             this.wersjaKarcianaToolStripMenuItem.Click += new System.EventHandler(this.wersjaKarcianaToolStripMenuItem_Click);
             // 
@@ -98,6 +100,11 @@
             this.ilośćRundToolStripMenuItem.Name = "ilośćRundToolStripMenuItem";
             this.ilośćRundToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.ilośćRundToolStripMenuItem.Text = "Ilość Rund";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
             // 
             // wyjdźToolStripMenuItem
             // 
@@ -126,15 +133,13 @@
             this.autorzyToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
             this.autorzyToolStripMenuItem.Text = "Autorzy";
             // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
-            // 
             // kostka1
             // 
             this.kostka1.Appearance = System.Windows.Forms.Appearance.Button;
             this.kostka1.AutoSize = true;
+            this.kostka1.Checked = true;
+            this.kostka1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.kostka1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.kostka1.Image = global::kosci.Properties.Resources._0;
             this.kostka1.Location = new System.Drawing.Point(46, 148);
             this.kostka1.Name = "kostka1";
@@ -147,6 +152,8 @@
             // 
             this.kostka2.Appearance = System.Windows.Forms.Appearance.Button;
             this.kostka2.AutoSize = true;
+            this.kostka2.Checked = true;
+            this.kostka2.CheckState = System.Windows.Forms.CheckState.Checked;
             this.kostka2.Image = global::kosci.Properties.Resources._0;
             this.kostka2.Location = new System.Drawing.Point(177, 148);
             this.kostka2.Name = "kostka2";
@@ -159,6 +166,8 @@
             // 
             this.kostka3.Appearance = System.Windows.Forms.Appearance.Button;
             this.kostka3.AutoSize = true;
+            this.kostka3.Checked = true;
+            this.kostka3.CheckState = System.Windows.Forms.CheckState.Checked;
             this.kostka3.Image = global::kosci.Properties.Resources._0;
             this.kostka3.Location = new System.Drawing.Point(308, 148);
             this.kostka3.Name = "kostka3";
@@ -171,6 +180,8 @@
             // 
             this.kostka4.Appearance = System.Windows.Forms.Appearance.Button;
             this.kostka4.AutoSize = true;
+            this.kostka4.Checked = true;
+            this.kostka4.CheckState = System.Windows.Forms.CheckState.Checked;
             this.kostka4.Image = global::kosci.Properties.Resources._0;
             this.kostka4.Location = new System.Drawing.Point(439, 148);
             this.kostka4.Name = "kostka4";
@@ -183,6 +194,8 @@
             // 
             this.kostka5.Appearance = System.Windows.Forms.Appearance.Button;
             this.kostka5.AutoSize = true;
+            this.kostka5.Checked = true;
+            this.kostka5.CheckState = System.Windows.Forms.CheckState.Checked;
             this.kostka5.Image = global::kosci.Properties.Resources._0;
             this.kostka5.Location = new System.Drawing.Point(570, 148);
             this.kostka5.Name = "kostka5";
@@ -191,18 +204,34 @@
             this.kostka5.UseVisualStyleBackColor = true;
             this.kostka5.Visible = false;
             // 
+            // rzuc
+            // 
+            this.rzuc.BackColor = System.Drawing.Color.Green;
+            this.rzuc.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.rzuc.Font = new System.Drawing.Font("Buxton Sketch", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rzuc.Location = new System.Drawing.Point(276, 305);
+            this.rzuc.Name = "rzuc";
+            this.rzuc.Size = new System.Drawing.Size(192, 78);
+            this.rzuc.TabIndex = 7;
+            this.rzuc.Text = "RZUĆ";
+            this.rzuc.UseVisualStyleBackColor = false;
+            this.rzuc.Visible = false;
+            this.rzuc.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::kosci.Properties.Resources.background_green;
             this.ClientSize = new System.Drawing.Size(742, 410);
+            this.Controls.Add(this.rzuc);
             this.Controls.Add(this.kostka5);
             this.Controls.Add(this.kostka4);
             this.Controls.Add(this.kostka3);
             this.Controls.Add(this.kostka2);
             this.Controls.Add(this.kostka1);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Kości";
@@ -231,6 +260,7 @@
         private System.Windows.Forms.CheckBox kostka3;
         private System.Windows.Forms.CheckBox kostka4;
         private System.Windows.Forms.CheckBox kostka5;
+        private System.Windows.Forms.Button rzuc;
     }
 }
 
--- kosci/Form1.cs
+++ kosci/Form1.cs
@@ -12,7 +12,8 @@ namespace kosci
 {
     public partial class Form1 : Form
     {
-        static int rodzaj_gry = 0;
+        static int rodzaj_gry = 0, runda = 0;
+        static int kostka1_los = 0, kostka2_los = 0, kostka3_los = 0, kostka4_los = 0, kostka5_los = 0;
 
         public Form1()
         {
@@ -25,18 +26,47 @@ namespace kosci
             Application.Exit();
         }
 
-        //silnik gry rozpoznaje ktorą werje rozgrywki wybrał użytkownik
-        private void silnik()
+        //wyswietlanie elementów po wybraniu rodzaju gry
+        private void pokaz()
         {
             kostka1.Visible = true;
             kostka2.Visible = true;
             kostka3.Visible = true;
             kostka4.Visible = true;
             kostka5.Visible = true;
+            rzuc.Visible = true;
+        }
 
+        //silnik gry rozpoznaje ktorą werje rozgrywki wybrał użytkownik
+        private void silnik()
+        {
+            Random r = new Random();
+            //losowanie liczb jeśli kostka jest wciśnięta
+            if (kostka1.Checked)
+            {
+                kostka1_los = r.Next(1,6);
+            }
+            if (kostka2.Checked)
+            {
+                kostka2_los = r.Next(1, 6);
+            }
+            if (kostka3.Checked)
+            {
+                kostka3_los = r.Next(1, 6);
+            }
+            if (kostka4.Checked)
+            {
+                kostka4_los = r.Next(1, 6);
+            }
+            if (kostka5.Checked)
+            {
+                kostka5_los = r.Next(1, 6);
+            }
+
+            //
             if (rodzaj_gry == 1)
             {
-                MessageBox.Show("Witaj w wersji standardowej");
+                MessageBox.Show("Wylosowałem: "+kostka1_los+" "+kostka2_los+" "+kostka3_los+" "+kostka4_los+" "+kostka5_los);
             }
             else if (rodzaj_gry == 2)
             {
@@ -47,13 +77,27 @@ namespace kosci
         private void wersjaToolStripMenuItem_Click(object sender, EventArgs e)
         {
             rodzaj_gry = 1;
-            silnik();
+            pokaz();
         }
 
         private void wersjaKarcianaToolStripMenuItem_Click(object sender, EventArgs e)
         {
             rodzaj_gry = 2;
-            silnik();
+            pokaz();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (runda > 3)
+            {
+                runda = 0;
+            }
+            else
+            {
+                MessageBox.Show("Runda nr: " + runda);
+                runda++;
+                silnik();
+            }
         }
     }
 }
